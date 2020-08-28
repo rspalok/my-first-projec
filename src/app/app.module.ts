004ccb5978556356f19c7de084436e803562c348
rspalok <34536048+rspalok@users.noreply.github.com>
2020-08-28 09:20:37 +0530

    alok 04

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,58 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TestComponent} from './test/test.component';
 import { HomeComponent } from './home/home.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ProfileComponent } from './profile/profile.component';  
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+//import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
+   
+import {MatSliderModule} from '@angular/material/slider';   
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule} from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [
     AppComponent,
     TestComponent,
-    HomeComponent
+    HomeComponent,
+    ProfileComponent, 
+   
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserModule,  
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    //HttpModule, 
+    MatSliderModule,
+    RouterModule,
+    MatCardModule, 
+    MatButtonModule,
+    MatMenuModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatCardModule
+  ], exports: [
+    MatSliderModule,
+    MatButtonModule,
+    MatMenuModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatCardModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { } 
+   
--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -1,4 +1,4 @@
-import { Component,OnInit } from '@angular/core';
+import { Component,OnInit } from '@angular/core';  
 
 @Component({
     selector: 'app-test', 
@@ -7,7 +7,6 @@ import { Component,OnInit } from '@angular/core';
 })
 export class TestComponent implements OnInit{
     constructor() { }
-
     ngOnInit(): void {
     }
 }
